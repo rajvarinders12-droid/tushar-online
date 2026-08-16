f7edbeb047df228f58580da716640e54ca045fb0
rajvarinders12-droid <rajvarinders12@gmail.com>
2026-08-17 02:10:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 targetElement.scrollIntoView({
                     behavior: 'smooth'
                 });
-            }
+             }
         });
     });
 
